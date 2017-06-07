f0f131d60adbbc8470645fa674140465fb42fb96
Kofemolka <kubovich@gmail.com>
2017-06-07 14:18:21 +0300

    no message

---
+++ external/lwip/src/timers/app_timer.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "app_timer.h"
+#include <stdlib.h>
+#include "nrf.h"
+#include "nrf_soc.h"
+#include "app_error.h"
+#include "nrf_delay.h"
+#include "app_util_platform.h"
+#include "sdk_common.h"
+
+uint32_t app_timer_cnt_get(void)
+{
+    return 0;
+}
